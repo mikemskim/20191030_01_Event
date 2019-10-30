dcfc769dbeae397487d9ee2d32aeed32b49c0113
mikemskim <mskim@steconsulting.co.kr>
2019-10-30 20:53:21 +0900

    이벤트 처리 연습 - ArrayList - 조건문 연습

--- app/src/main/java/com/tj/a20191030_01_event/MainActivity.kt
+++ app/src/main/java/com/tj/a20191030_01_event/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tj.a20191030_01_event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        firstTextView.text = "기백2다"
+        var strTest = "기백2다"
+        firstTextView.text = strTest
+
+        okBtn.setOnClickListener {
+            firstTextView.text = "확인 버튼 클릭 됨"
+        }
+
+        okBtn.setOnLongClickListener {
+            firstTextView.text = "님 좀 짱인듯!"
+            return@setOnLongClickListener true
+        }
+    // @라벨 에 대한 예시, 책 49p
+
+        firstFor@ for (i in 0..10){
+            secondFor@ for (j in 0..10) {
+                Log.d("코틀린 기초", "$i $j")
+                if(j==2) {
+                    break@firstFor
+                }
+            }
+        }
+
+        var stringListTemp = ArrayList<String>()
+        stringListTemp.add("AAA")
+//        stringListTemp.add(333) // 에러 발생
+
+        var stringList:ArrayList<String> = ArrayList()
+
+        stringList.add("조근택")
+        stringList.add("한용수")
+        stringList.add("유호인")
+
+        var intList:ArrayList<Int> = ArrayList()
+
+        intList.add(10)
+        intList.add(20)
+        intList.add(30)
+
+        val indexOfCho = stringList.indexOf("조근택");
+
+        Log.d("코틀린 기초","조근택의 위치 : $indexOfCho")
+
+        var a = 10
+        var b = 20
+
+        var c = if (a == b) {
+            Log.d("코틀린 기초", "같음")
+        } else {
+            Log.d("코틀린 기초", "다름")
+        } // 이런 문법도 가능함, 책 39p
+
+        when(a) {
+            0, 1 -> Log.d("코틀린 기초", "a가 0이나 1이다")
+            in 2..5 -> Log.d("코틀린 기초", "a가 2~5 사이")
+            in intList -> Log.d("코틀린 기초", "a가 intList에 포함")
+            !in intList -> Log.d("코틀린 기초", "a가 intList에 불포함")
+        }
+
+
     }
+
 }
